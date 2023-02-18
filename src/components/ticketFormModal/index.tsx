504df3152b32ee046fb751c5a695dc5d45b575b2
matveyb <matvey.b2022@gmail.com>
2023-02-18 16:19:09 +0100

    create ticket form update

---
+++ src/components/ticketFormModal/index.tsx
@@ -0,0 +1,106 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { createRef, useState } from "react";
+
+import {
+  Group,
+  ModalPage,
+  ModalPageHeader,
+  PanelHeaderClose,
+  usePlatform,
+  useAdaptivityConditionalRender,
+  PanelHeaderButton,
+  FormItem,
+  Input,
+  Platform,
+  Button,
+  Div
+} from "@vkontakte/vkui";
+
+import { Icon24Dismiss } from "@vkontakte/icons";
+
+interface IProps {
+  id: string | undefined;
+  onClose: () => void;
+  onSubmit: (vals: any) => void;
+  dynamicContentHeight: boolean;
+}
+
+const DynamicModalPage: React.FC<IProps> = ({
+  onClose,
+  onSubmit,
+  ...props
+}) => {
+  const platform = usePlatform();
+  const { sizeX } = useAdaptivityConditionalRender();
+  const [ticketName, setTicketName] = useState<string>("");
+  const [ticketAmount, setTicketAmount] = useState<string>("");
+
+  const submit = () => {
+    console.log(ticketName, ticketAmount, "ticketName ticketsAmount");
+    onSubmit({
+      name: ticketName,
+      amount: ticketAmount
+    });
+  };
+
+  return (
+    <ModalPage
+      {...props}
+      header={
+        <ModalPageHeader
+          before={
+            sizeX.compact &&
+            platform === Platform.ANDROID && (
+              <PanelHeaderClose
+                className={sizeX.compact.className}
+                onClick={onClose}
+              />
+            )
+          }
+          after={
+            sizeX.compact &&
+            platform === Platform.IOS && (
+              <PanelHeaderButton
+                className={sizeX.compact.className}
+                onClick={onClose}
+              >
+                <Icon24Dismiss />
+              </PanelHeaderButton>
+            )
+          }
+        >
+          Введите данные события
+        </ModalPageHeader>
+      }
+    >
+      <Group>
+        <FormItem top="Имя">
+          <Input
+            onChange={(e) => setTicketName(e.currentTarget.value)}
+            type="text"
+            placeholder="Введите имя"
+          />
+        </FormItem>
+        <FormItem top="Кол-во">
+          <Input
+            type="text"
+            placeholder="Введите кол-во"
+            onChange={(e) => setTicketAmount(e.currentTarget.value)}
+          />
+        </FormItem>
+      </Group>
+      <Div style={{ width: "100%", textAlign: "right" }}>
+        <Button
+          style={{ marginRight: "45px" }}
+          size="l"
+          mode="primary"
+          onClick={submit}
+        >
+          Создать
+        </Button>
+      </Div>
+    </ModalPage>
+  );
+};
+
+export default DynamicModalPage;
--- src/panels/createTicket/index.tsx
+++ src/panels/createTicket/index.tsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState, createRef, useCallback } from "react";
 
 import {
   Panel,
-  Button,
+  Select,
   Group,
   CardGrid,
   Card,
   PanelHeaderBack,
-  PanelHeader
+  PanelHeader,
+  FormLayout,
+  FormItem,
+  Text,
+  DateInput,
+  LocaleProvider,
+  Input,
+  CellButton,
+  Button,
+  Div,
+  ModalRoot
 } from "@vkontakte/vkui";
+import TicketFormModal from "@/components/ticketFormModal";
 
 import "./index.css";
+import { Icon28AddOutline } from "@vkontakte/icons";
 
 interface IHomeProps {
   id: string;
@@ -18,14 +30,167 @@ interface IHomeProps {
 }
 
 const CreateTickets: React.FC<IHomeProps> = ({ id, onBack }) => {
+  const [purpose, setPurpose] = useState("");
+  const [ticketsData, setTicketsData] = useState<any>([]);
+  const [activeModal, setActiveModal] = React.useState<string | undefined>(
+    undefined
+  );
+  const [value, setValue] = useState<Date | undefined>(() => new Date());
+  const textInput = createRef<HTMLInputElement>();
+  const [locale, setLocale] = useState("ru");
+
+  const onChange = (e: any) => {
+    const { name, value } = e.currentTarget;
+
+    setPurpose(value);
+  };
+
+  const toggleModal = () => {
+    setActiveModal(activeModal ? undefined : "createTicketModal");
+  };
+
+  const submitFullTicket = () => {
+    console.log("submitFullTicket");
+  };
+
   return (
     <Panel id={id}>
       <PanelHeader before={<PanelHeaderBack onClick={onBack} />}>
         Добавление события
       </PanelHeader>
-      <Group mode="plain">
+      <div style={{ zIndex: 1 }}>
+        <ModalRoot activeModal={activeModal} onClose={toggleModal}>
+          <TicketFormModal
+            id={activeModal}
+            onSubmit={(val: any) => {
+              const tickets = ticketsData ? [...ticketsData, val] : [val];
+              toggleModal();
+              setTicketsData(tickets);
+            }}
+            onClose={toggleModal}
+            dynamicContentHeight
+          />
+        </ModalRoot>
+      </div>
+      <Group mode="plain" style={!activeModal ? { zIndex: 2 } : undefined}>
         <CardGrid size="l">
-          <Card className="card_wrap">Создать событие</Card>
+          <Card className="card_wrap">
+            <CardGrid size="l" className="card_wrap__form-inner">
+              <Card
+                mode="shadow"
+                style={{ height: "100%", overflowY: "auto", padding: "20px 0" }}
+              >
+                <FormLayout style={{ width: "100%" }}>
+                  <FormItem
+                    className="form-row"
+                    status={purpose ? "valid" : "error"}
+                  >
+                    <Text style={{ marginRight: "10px" }}>Организатор</Text>
+                    <Select
+                      placeholder="Выберите цель поездки"
+                      onChange={onChange}
+                      value={purpose}
+                      name="purpose"
+                      options={[
+                        {
+                          value: "0",
+                          label: "Бизнес или работа"
+                        },
+                        {
+                          value: "1",
+                          label: "Индивидуальный туризм"
+                        },
+                        {
+                          value: "2",
+                          label: "Посещение близких родственников"
+                        }
+                      ]}
+                    />
+                  </FormItem>
+                  <FormItem className="form-row">
+                    <Text style={{ marginRight: "10px" }}>Локация</Text>
+                    <Input
+                      style={{ minWidth: "340px" }}
+                      getRef={textInput}
+                      type="text"
+                      placeholder="Введите адрес или ссылку на событие"
+                    />
+                  </FormItem>
+                  <FormItem className="form-row">
+                    <Text style={{ marginRight: "10px" }}>Дата и время</Text>
+                    <LocaleProvider value={locale}>
+                      <DateInput
+                        value={value}
+                        onChange={setValue}
+                        enableTime
+                        closeOnChange
+                      />
+                    </LocaleProvider>
+                  </FormItem>
+                  <FormItem className="form-row">
+                    <Text style={{ marginRight: "10px" }}>Билеты</Text>
+                    <Div style={{ width: "fit-content", padding: 0 }}>
+                      <CellButton
+                        style={{
+                          margin: "0 0 10px 0",
+                          boxSizing: "border-box"
+                        }}
+                        onClick={() => setActiveModal("createTicketModal")}
+                        centered
+                        before={<Icon28AddOutline />}
+                      >
+                        Добавить билет
+                      </CellButton>
+
+                      {ticketsData?.length
+                        ? ticketsData.map((_: any, index: number) => {
+                            return (
+                              <Group>
+                                <div style={{ display: "flex" }}>
+                                  <Div>
+                                    <Text width={500} size={16}>
+                                      Имя
+                                    </Text>
+                                    <Text style={{ fontSize: "12px" }}>
+                                      {_.name}
+                                    </Text>
+                                  </Div>
+
+                                  <Div>
+                                    <Text width={500} size={16}>
+                                      Кол-во
+                                    </Text>
+                                    <Text style={{ fontSize: "12px" }}>
+                                      {_.amount}
+                                    </Text>
+                                  </Div>
+                                </div>
+                              </Group>
+                            );
+                          })
+                        : null}
+                    </Div>
+                  </FormItem>
+                  <Div
+                    style={{
+                      width: "70%",
+                      margin: "0 auto",
+                      textAlign: "right"
+                    }}
+                  >
+                    <Button
+                      style={{ marginLeft: "auto" }}
+                      size="l"
+                      mode="primary"
+                      onClick={submitFullTicket}
+                    >
+                      Создать
+                    </Button>
+                  </Div>
+                </FormLayout>
+              </Card>
+            </CardGrid>
+          </Card>
         </CardGrid>
       </Group>
     </Panel>
